2a1ddd373a353267d1968def9c3704a0791cd54a
Juan Jose Gallego <juan.gallego.calderon@correounivalle.edu.co>
2023-12-14 16:29:21 -0500

    las primeras 3 primeras soluciones en perfecto estado

--- app/src/main/scala/taller4/Taller4.scala
+++ app/src/main/scala/taller4/Taller4.scala
@@ -16,105 +16,105 @@ object Taller4 {
   val alfabeto = Seq('a', 'c', 'g', 't')
   type Oraculo = Seq[Char] => Boolean
 
+
+  //Esta funcion genera una secuencia aleatoria de tamaño n (prueba de 3 millones de elementos)
   def secuenciaaleatoria(tamano: Int): String = {
     val random = new Random()
-    (1 to tamano).map(_ => alfabeto(random.nextInt(alfabeto.length))).mkString
+    (1 to tamano).map(_ => alfabeto(random.nextInt(alfabeto.length))).mkString //En cada posición se pega una letra de forma aleatoria
   }
 
 
-
+//Prc = Problema en la reconstruccion de cadenas ingenuo
   def prc_ingenuo(alfabeto: Seq[Char], tamano: Int, o: Oraculo): Seq[Char] = {
     def cadenas_candidatas(alfabeto: Seq[Char], tamano: Int): Seq[Seq[Char]] = {
-      if (tamano == 0) {
-        Seq(Seq.empty[Char])
-      } else {
+      if (tamano == 0) Seq(Seq.empty[Char]) //caso base
+       else { //longitudes mayores a 0 (caso recursivo)
         alfabeto.flatMap(caracter => cadenas_candidatas(alfabeto, tamano - 1).map(caracter +: _))
       }
     }
+
     val combinaciones = cadenas_candidatas(alfabeto,tamano)
-    combinaciones.flatMap { seq =>
-      if (o(seq)) seq
-      else None
-    }
+    //Itera entre los elementos de la secuencia y si cumple con la condicion del oraculo, retorna la secuencia.
+    combinaciones.flatMap { seq =>      if (o(seq)) seq      else None}
+
   }
 
   def prc_mejorado(alfabeto: Seq[Char], tamano: Int, o:Oraculo): Seq[Char] = {
     def subcaden_candidatas(k: Int, SC: Seq[Seq[Char]]): Seq[Seq[Char]] = {
-      if (k > tamano) SC
+      if (k == tamano) SC
       else {
-        subcaden_candidatas(k + 1, SC.flatMap(subc => alfabeto.map(letra => subc :+ letra)).filter(o))
+        val sck = SC.flatMap(elementos_sc => alfabeto.map(letra => elementos_sc :+ letra)).filter(o)
+        subcaden_candidatas(k + 1, sck)
       }
     }
 
-    val SC = subcaden_candidatas(1, Seq(Seq()))
-    SC.find(_.length == tamano).getOrElse(Seq())
+    val SC = subcaden_candidatas(0, Seq(Seq()))
+    SC.head
   }
 
   def prc_turbo(alfabeto: Seq[Char], tamano: Int, o:Oraculo): Seq[Char] = {
-    def subcaden_candidataspar(k: Int, n:Int, SC: Seq[Seq[Char]]): Seq[Seq[Char]] = {
-      if (k > tamano) SC
+    def subcaden_candidatas(k: Int, SC: Seq[Seq[Char]]): Seq[Seq[Char]] = {
+      if (k >= tamano) SC
       else {
-        val n = k*2
-        val SCk = SC.flatMap(subc => alfabeto.map(letra => subc :+ letra)).filter(o)
-        subcaden_candidataspar(k+1 , n, SCk)
+        val SCk = SC.flatMap { s1 =>
+          SC.flatMap { s2 =>
+            Seq(s1 ++ s2)
+          }
+        }
+        val SCkFiltrado = SCk.filter(o)
+        subcaden_candidatas(k*2 , SCkFiltrado)
       }
     }
 
-    val SC = subcaden_candidataspar(1, 1, Seq(Seq.empty[Char]))
-    SC.find(_.length == tamano).getOrElse(Seq())
+    val SC = subcaden_candidatas(1, alfabeto.map(Seq(_)))
+    SC.head
 
   }
 
   //FALTA MEJORARLO
-  def prc_turboMejorado(alfabeto: Seq[Char], tamano: Int, o:Oraculo): Seq[Char] = {
-
-    def subcaden_candidataspar(k: Int, n: Int, SC: Seq[Seq[Char]]): Seq[Seq[Char]] = {
-      if (k > tamano) SC
-      else {
-        val n = k * 2
-        val SCk = SC.flatMap(subc => alfabeto.map(letra => subc :+ letra)).filter(o)
-        subcaden_candidataspar(k + 1, n, SCk)
-      }
-    }
-
-    val SC = subcaden_candidataspar(1, 1, Seq(Seq.empty[Char]))
-    SC.find(_.length == tamano).getOrElse(Seq())
-
-  }
-
+  //def prc_turboMejorado(alfabeto: Seq[Char], tamano: Int, o:Oraculo): Seq[Char] = {
+  //}
 
     def main(args: Array[String]): Unit = {
-      val secuencia = Seq('a', 'c', 'a', 'c','g','t')
+
+      val secuencia = Seq('a', 'c', 'a', 'g')
+      val tamano = 8
+      val secuenciaRandom = secuenciaaleatoria(tamano)
 
       val o: Oraculo = (s: Seq[Char]) => {
-        secuencia.containsSlice(s)
+        secuenciaRandom.containsSlice(s)
       }
       val inicio1 = System.nanoTime()
-      val cadena = prc_ingenuo(alfabeto, 6, o)
+      val cadena = prc_ingenuo(alfabeto, tamano, o)
       println(s" ingenuo Cadena encontrada: $cadena")
       val fin1 = System.nanoTime()
       val tiempo1 = (fin1 - inicio1)/ 1e6
-      println(s"Tiempo de ejecucion: $tiempo1 ms")
 
       val inicio2 = System.nanoTime()
-      val cadenaM = prc_mejorado(alfabeto, 6, o)
+      val cadenaM = prc_mejorado(alfabeto, tamano, o)
       println(s" mejorado Cadena encontrada: $cadenaM")
       val fin2 = System.nanoTime()
       val tiempo2 = (fin2 - inicio2)/ 1e6
-      println(s"Tiempo de ejecucion: $tiempo2 ms")
 
       val inicio3 = System.nanoTime()
-      val cadenaT = prc_turbo(alfabeto, 6, o)
+      val cadenaT = prc_turbo(alfabeto, tamano, o)
       println(s" turbo Cadena encontrada: $cadenaT")
       val fin3 = System.nanoTime()
       val tiempo3 = (fin3 - inicio3)/ 1e6
-      println(s"Tiempo de ejecucion: $tiempo3 ms")
 
       val inicio4 = System.nanoTime()
-      //val cadenaTM = prc_turboMejorado(alfabeto, 6, o)
+      //val cadenaTM = prc_turboMejorado(alfabeto, tamano, o)
       //println(s" turbo Mejorado Cadena encontrada: $cadenaTM")
       val fin4 = System.nanoTime()
       val tiempo4 = (fin4 - inicio4) / 1e6
-      println(s"Tiempo de ejecucion: $tiempo4 ms")
+
+
+
+
+      println(s"Tiempo de ejecucion: $tiempo1 ms")
+      println(s"Tiempo de ejecucion: $tiempo2 ms")
+      println(s"Tiempo de ejecucion: $tiempo3 ms")
+      //println(s"Tiempo de ejecucion: $tiempo4 ms")
   }
 }
+
